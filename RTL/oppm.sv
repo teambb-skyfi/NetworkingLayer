1136ac7717ae303a99ff0f12308276f40172ae65
Syed Raziq Mohideen <smohidee@andrew.cmu.edu>
2019-03-08 23:00:34 -0500

    Clocking block for testing

--- RTL/oppm.sv
+++ RTL/oppm.sv
@@ -1,16 +1,18 @@
 //---- Pulser
 // Drive a pulse of defined length
 module Pulser
+#(parameter COUNT)
  (input  logic clk,    // Clock
   input  logic rst_n,  // Asynchronous reset active low
   input  logic start,  // Start pulse
   output logic avail,  // Pulser available
   output logic pulse   // Output pulse
 );
+  parameter N_CT = $clog2(COUNT+1);
 
-  logic [3:0] count;
-  logic       clear, up;
-  Counter pulseCounter(.D(4'b0), .load(clear), .Q(count), .*);
+  logic [N_CT-1:0] count;
+  logic            clear, up;
+  Counter #(.WIDTH(N_CT)) pulseCounter(.D(4'b0), .load(clear), .Q(count), .*);
 
   // State register
   enum {IDLE, PULSE} s, ns;
@@ -32,11 +34,11 @@ module Pulser
         pulse = 1'b0;
       end
       PULSE: begin
-        ns = (count < 10) ? PULSE : IDLE;
-        clear = (count >= 10);
-        up = (count < 10);
+        ns = (count < COUNT) ? PULSE : IDLE;
+        clear = (count >= COUNT);
+        up = (count < COUNT);
         avail = 1'b0;
-        pulse = (count < 10);
+        pulse = (count < COUNT);
       end
     endcase
   end
@@ -54,15 +56,25 @@ module Pulser_test;
   logic start;
   logic avail;
   logic pulse;
-  Pulser dut(.*);
+  Pulser #(.COUNT(10)) dut(.*);
+
+  default clocking cb @(posedge clk);
+    default input #1step output #2;
+    output negedge rst_n;
+    output start;
+    input  avail;
+    input  pulse;
+  endclocking: cb
 
   initial begin
-    rst_n  =     1'b0;
-    start <=     1'b0;
-    rst_n <=  #1 1'b1;
-    start <= #19 1'b1;
-    start <= #39 1'b0;
+    rst_n = 1'b0;
+    start = 1'b0;
 
-    #151 $finish;
+    ##1 cb.rst_n <= 1'b1;
+    ##1 cb.start <= 1'b1;
+    ##1 cb.start <= 1'b0;
+    @(negedge cb.pulse);
+
+    ##1 $finish;
   end
 endmodule: Pulser_test
