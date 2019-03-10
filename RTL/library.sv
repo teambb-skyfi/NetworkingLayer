770e35a49e9c0774534d00bdd2efaf8b6fd56cfa
Syed Raziq Mohideen <smohidee@andrew.cmu.edu>
2019-03-09 21:45:28 -0500

    v1 of Modulator

--- RTL/library.sv
+++ RTL/library.sv
@@ -1,3 +1,5 @@
+`default_nettype none
+
 //---- Counter
 // Register with count-up capability
 module Counter
@@ -13,3 +15,18 @@ module Counter
       Q <= Q + INCR;
   end
 endmodule: Counter
+
+//---- Register
+// Stores a value in memory
+module Register
+  #(parameter WIDTH = 4, DEFVAL = 0)
+  (input  logic [WIDTH-1:0] D,
+   input  logic             en, clk, rst_n,
+   output logic [WIDTH-1:0] Q);
+
+  always_ff @(posedge clk, negedge rst_n) begin
+    if (~rst_n) Q <= DEFVAL;
+    else if (en) Q <= D;
+  end
+
+endmodule : Register
--- RTL/oppm.sv
+++ RTL/oppm.sv
@@ -1,18 +1,21 @@
+`default_nettype none
+
 //---- Pulser
 // Drive a pulse of defined length
 module Pulser
-#(parameter COUNT)
- (input  logic clk,    // Clock
-  input  logic rst_n,  // Asynchronous reset active low
-  input  logic start,  // Start pulse
-  output logic avail,  // Pulser available
-  output logic pulse   // Output pulse
+#(parameter COUNT     // Pulse width in clock ticks
+)
+ (input  logic clk,   // Clock
+  input  logic rst_n, // Asynchronous reset active low
+  input  logic start, // Start pulse
+  output logic avail, // Pulser available
+  output logic pulse  // Output pulse
 );
-  parameter N_CT = $clog2(COUNT+1);
+  localparam N_SZ = $clog2(COUNT+1);
 
-  logic [N_CT-1:0] count;
+  logic [N_SZ-1:0] count;
   logic            clear, up;
-  Counter #(.WIDTH(N_CT)) pulseCounter(.D({N_CT{1'b0}}), .load(clear),
+  Counter #(.WIDTH(N_SZ)) pulseCounter(.D({N_SZ{1'b0}}), .load(clear),
                                        .Q(count), .*);
 
   // State register
@@ -84,3 +87,103 @@ module Pulser_test;
     ##1 $finish;
   end
 endmodule: Pulser_test
+
+//---- Modulator
+// Modulates 
+module Modulator
+#(parameter PULSE_CT, // Pulse width in clock ticks
+            N,        // Data size in bits
+            L         // Time slot size in clock ticks
+)
+ (input  logic         clk,   // Clock
+  input  logic         rst_n, // Asynchronous reset active low
+  input  logic [N-1:0] data,  // Data to modulate
+  input  logic         valid, // Data is valid
+  output logic         avail, // Data can be latched
+  output logic         pulse  // Output pulse
+);
+
+  localparam L_SZ = $clog2(L+1);
+  logic [L_SZ-1:0] slot_ct;
+  logic            slot_clear, slot_up;
+  Counter #(.WIDTH(L_SZ)) slotCounter(.D({L_SZ{1'b0}}), .load(slot_clear),
+                                      .up(slot_up), .Q(slot_ct), .*);
+  assign slot_clear = (slot_ct == L-1);
+  assign slot_up = (slot_ct != L-1);
+
+  localparam SYMBOL_CT = 2**N - 1;
+  localparam SYMBOL_SZ = N;
+  logic [SYMBOL_SZ-1:0] symbol_id;
+  logic                 symbol_clear, symbol_up;
+  Counter #(.WIDTH(SYMBOL_SZ)) symbolCounter(.D({SYMBOL_SZ{1'b0}}),
+                                             .load(symbol_clear),
+                                             .up(symbol_up), .Q(symbol_id), .*);
+
+  logic final_symbol;
+  assign final_symbol = symbol_id == SYMBOL_CT;
+
+  assign symbol_clear = slot_clear && final_symbol;
+  assign symbol_up = slot_clear && !final_symbol;
+
+  logic         data_en;
+  logic [N-1:0] data_D, data_Q;
+  Register #(.WIDTH(N)) dataReg(.D(data_D), .en(data_en), .Q(data_Q), .*);
+
+  assign data_en = symbol_clear;
+  assign data_D = valid ? data : {N{1'b0}};
+
+  assign avail = symbol_clear;
+
+  logic pulser_start, pulser_avail;
+  Pulser #(.COUNT(PULSE_CT)) pulser(.start(pulser_start), .avail(pulser_avail),
+                                    .*);
+
+  assign pulser_start = (slot_ct == 0) && (data_Q == symbol_id);
+endmodule: Modulator
+
+// Modulator testbench
+module Modulator_test;
+  logic clk;
+  initial begin
+    clk = 1'b1;
+    forever #5 clk = ~clk;
+  end
+
+  localparam int SEED = 18500;
+  initial $srandom(SEED);
+
+  localparam PULSE_CT = 2;
+  localparam N = 2;
+  localparam L = 4;
+  logic         rst_n;
+  logic [N-1:0] data;
+  logic         valid;
+  logic         avail;
+  logic         pulse;
+  Modulator #(.PULSE_CT(PULSE_CT), .N(N), .L(L)) dut(.*);
+
+  default clocking cb @(posedge clk);
+    default input #1step output #2;
+    output negedge rst_n;
+    output data;
+    output valid;
+    input  avail;
+    input  pulse;
+  endclocking: cb
+
+  initial begin
+    rst_n = 1'b0;
+    data = 2'b1;
+    valid = 1'b1;
+
+    ##1 cb.rst_n <= 1'b1;
+
+    repeat (10) begin
+      @(negedge cb.pulse);
+      cb.data <= $urandom;
+    end
+
+    @(posedge cb.avail);
+    ##1 $finish;
+  end
+endmodule: Modulator_test
